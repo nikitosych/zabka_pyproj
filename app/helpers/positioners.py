82a71f279ffb07224e7b218352099d6bea84c603
nikitosych <boriszyn@gmail.com>
2025-05-28 21:31:06 +0200

    Added packer for h2 labels

--- app/helpers/positioners.py
+++ app/helpers/positioners.py
@@ -3,6 +3,9 @@ from customtkinter import *
 def h1(label: CTkLabel):
     label.pack(ipady=15, ipadx=25)
 
+def h2(label: CTkLabel):
+    label.pack(ipady=12, ipadx=25)
+
 def nest(inner: CTkFrame):
     inner.place(relx=0.5, rely=0.5, anchor="center")
 
