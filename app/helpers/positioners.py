f1eb43968d992286bc1b75cd7cf4c3b9cd3349cc
nikitosych <boriszyn@gmail.com>
2025-05-28 22:32:07 +0200

    Small tweaks

--- app/helpers/positioners.py
+++ app/helpers/positioners.py
@@ -1,13 +1,13 @@
 from customtkinter import *
 
-def h1(label: CTkLabel):
-    label.pack(ipady=15, ipadx=25)
+def h1(label: CTkLabel, **kwargs):
+    label.pack(ipady=15, ipadx=25, **kwargs)
 
-def h2(label: CTkLabel):
-    label.pack(ipady=12, ipadx=25)
+def h2(label: CTkLabel, **kwargs):
+    label.pack(ipady=12, ipadx=25, **kwargs)
 
-def nest(inner: CTkFrame):
-    inner.place(relx=0.5, rely=0.5, anchor="center")
+def nest(inner: CTkFrame, **kwargs):
+    inner.place(relx=0.5, rely=0.5, anchor="center", **kwargs)
 
-def btn(button: CTkButton):
-    button.pack(ipady=5,ipadx=8, padx=10, pady=7)
+def btn(button: CTkButton, **kwargs):
+    button.pack(ipady=5,ipadx=8, padx=10, pady=7, **kwargs)
